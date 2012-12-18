d5e7518e3a646c2f500c8b1656ce04f084c631d1
Anant Narayanan <anant@kix.in>
2012-12-17 17:43:08 -0800

    Update README and add rules.json

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
-firepano
+
+Firepano
 ========
 
 A simple client-side photo sharing site.
+
+### [Live Demo](http://firebase.github.com/firepano/) | [Example Panorama](http://firebase.github.com/firepano/#70ed37564320f9f4f8d753ae31fb6c1fad0bcc7630a607b6e8ee7e7cfbb650e4)
+
+How does it work?
+-----------------
+The security model of this simple photo sharing app is predicated on having
+locations being unguessable. We construct paths into Firebase using a hash of
+the file being uploaded. Then, anyone that has access to the share-able link
+can then lookup the location in Firebase and view its contents. 
+
+A simple rule set is required to make sure none of the keys are enumerable
+from Firebase. This prevents retrieval of the keys from any of the Firebase
+clients, including REST endpoints. We also add a write rule to the photos so
+that once the data has been written, no one can override or delete data that
+already exists. The rules for this application are in [rules.json](http://github.com/firebase/firepano/blob/gh-pages/rules.json).
+
+Exercises for the reader
+------------------------
+  1. Add a chat/comment system to each photo.
+  2. Support logging in with Facebook/Twitter to manage photos: edits, removals, etc.
+  3. Add metadata to indicate public/private photos.
+  4. Add a realtime feed of newly uploaded photos.
+
+License
+-------
+No part of this project may be copied, modified, propagated, or distributed
+except according to terms in the accompanying LICENSE file.
--- firepano.js
+++ firepano.js
@@ -1,48 +1,6 @@
-/* 
- * Firepano: A simple client-side photo sharing site.
- *
- * Live demo: http://firepano.misc.firebase.com/
- * An example panorama: http://firepano.misc.firebase.com/#70ed37564320f9f4f8d753ae31fb6c1fad0bcc7630a607b6e8ee7e7cfbb650e4
- *
- * The security model of this simple photo sharing app is predicated on having
- * locations being unguessable. We construct paths into Firebase using a hash of
- * the file being uploaded. Then, anyone that has access to the share-able link
- * can then lookup the location in Firebase and view its contents. 
- *
- * A simple rule set is required to make sure none of the keys are enumerable
- * from Firebase. This prevents retrieval of the keys from any of the Firebase
- * clients, including REST endpoints. We also add a write rule to the photos so
- * that once the data has been written, no one can override or delete data that
- * already exists.
- *
- * The Firebase rules:
-
-{
-  "rules": {
-    ".read": false,
-    ".write": false,
-    "pano": {
-     ".read": false,
-     ".write": false,
-      "$pano": {
-       ".read": true,
-       ".write": "! data.exists()"
-      }
-    }
-  }
-}
-
- *
- * Exercises for the reader:
- *  1. Add a chat/comments system to each photo.
- *  2. Support logging in with Facebook/Twitter to manage photos: edits, removals, etc.
- *  3. Add metadata to indicate public/private photo; add a realtime feed of newly uploaded photos.
- *
- */
-
-var firebaseRef = 'https://firepano.firebaseio.com/';
 
 var spinner = new Spinner({color: '#ddd'});
+var firebaseRef = 'https://firepano.firebaseio.com/';
 
 function handleFileSelect(evt) {
   var f = evt.target.files[0];
@@ -59,7 +17,7 @@ function handleFileSelect(evt) {
         spinner.stop();
         document.getElementById("pano").src = e.target.result;
         $('#file-upload').hide();
-	// Update the location bar so the URL can be shared with others
+        // Update the location bar so the URL can be shared with others
         window.location.hash = hash;
       });
     };
@@ -73,21 +31,19 @@ $(function() {
 
   var idx = window.location.href.indexOf('#');
   var hash = (idx > 0) ? window.location.href.slice(idx + 1) : '';
-  if(hash === '') {
-    // No hash found, so render the file upload button
+  if (hash === '') {
+    // No hash found, so render the file upload button.
     $('#file-upload').show();
     document.getElementById("file-upload").addEventListener('change', handleFileSelect, false);
-  }
-  else {
-    // A hash was passed in, so let's retrieve and render it
+  } else {
+    // A hash was passed in, so let's retrieve and render it.
     spinner.spin(document.getElementById('spin'));
     var f = new Firebase(firebaseRef + '/pano/' + hash + '/filePayload');
     f.once('value', function(snap) {
       var payload = snap.val();
-      if(payload != null) {
+      if (payload != null) {
         document.getElementById("pano").src = payload;
-      }
-      else {
+      } else {
         $('#body').append("Not found");
       }
       spinner.stop();
